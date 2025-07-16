4effd2cfe57a6ceacc50ee726874ca69453af14e
huangyu <598050554@qq.com>
2025-07-16 17:09:11 +0800

    add

--- src/main.js
+++ src/main.js
@@ -2,14 +2,22 @@ import './reset.css'
 import './style.css'
 import 'cesium/Source/Widgets/widgets.css'
 import * as Cesium from 'cesium'
+import { Cartesian3, createOsmBuildingsAsync, Ion, Math as CesiumMath, Terrain, Viewer } from 'cesium';
 
 
-try { 
+try {
   // 初始化 Cesium Viewer
-  Cesium.Ion.defaultAccessToken ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNGNkZmExNi1iNGFjLTRmMWQtYTk0YS1kZDA0YThjODg0YWEiLCJpZCI6MTIzMzI5LCJpYXQiOjE3NTI2NTYwMDV9.AGrRQMfnLy7_rqCkCqt0ESx3NX3ulhfOZLv-sDZB-vA'
-  const viewer = new Cesium.Viewer('app', {
+  Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJiNGNkZmExNi1iNGFjLTRmMWQtYTk0YS1kZDA0YThjODg0YWEiLCJpZCI6MTIzMzI5LCJpYXQiOjE3NTI2NTYwMDV9.AGrRQMfnLy7_rqCkCqt0ESx3NX3ulhfOZLv-sDZB-vA'
+  const viewer = new Viewer('app', {
     infoBox: false, // 禁用 InfoBox
-  });   
+  });
+  viewer.camera.flyTo({
+    destination: Cartesian3.fromDegrees(104.063981, 30.659599, 600),
+    orientation: {
+      heading: CesiumMath.toRadians(0.0),
+      pitch: CesiumMath.toRadians(-15.0),
+    }
+  });
 } catch (error) {
   console.log(error)
 }
